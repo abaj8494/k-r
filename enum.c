a991c6e54d02dbf89b2299ceb7f490ea57557679
Aayush Bajaj <aayushbajaj7@gmail.com>
2023-02-23 20:48:32 +1100

    enums

---
+++ enum.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+
+enum boolean {ON, OFF};
+enum months {JAN = 1, FEB, MAR, APR, MAY, JUN, JUL, AUG,
+             SEP, OCT, NOV, DEC};
+
+int main()
+{
+    printf("%d\n", OFF);
+    printf("%d\n", OCT);
+}
