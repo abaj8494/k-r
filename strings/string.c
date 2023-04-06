5aa956bb17bab44792ae776a808f28ce6a838036
manjaro <j@abaj.io>
2023-04-07 00:16:01 +1000

    testing

--- strings/string.c
+++ strings/string.c
@@ -7,3 +7,10 @@ void strcpy(char *a, char *b)
 {
 	for (i = 0; (a[i] = b[i]) != 0; i++) ;
 }
+
+int strcmp(char *a, char *b)
+{
+	for (i = 0; a[i] == b[i]; i++)
+		if (a[i] == 0) return 0;
+	return a[i] - b[i];
+}
